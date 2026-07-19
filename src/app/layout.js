09639c482836556e29c2a321132d68f6dad45899
Sardar170 <faisalsardar170@gmail.com>
2026-07-19 15:04:03 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,21 @@
 "use client";
 
 import { useEffect } from "react";
-import Script from "next/script";
 
 export default function RootLayout({ children }) {
   useEffect(() => {
-    // Jab window load ho jaye ya component mount ho, tab script ko init karein
-    const initBotpress = () => {
+    // 1. First Script (Shareable)
+    const script1 = document.createElement("script");
+    script1.src = "https://cdn.botpress.cloud/webchat/v2.2/shareable.js";
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    // 2. Second Script (Bundle & Init)
+    const script2 = document.createElement("script");
+    script2.src = "https://styler-e59b32.botpress.cloud/webchat/v2.2/bundle.js";
+    script2.async = true;
+    
+    script2.onload = () => {
       if (window.botpress) {
         window.botpress.init({
           "botId": "05aff31d-5226-4a81-846a-59c4710120a9",
@@ -20,16 +29,11 @@ export default function RootLayout({ children }) {
       }
     };
 
-    // Agar botpress pehle se load ho chuka hai
-    if (window.botpress) {
-      initBotpress();
-    } else {
-      // Script load hone ka wait karein
-      window.addEventListener("botpressWidgetReady", initBotpress);
-    }
+    document.body.appendChild(script2);
 
     return () => {
-      window.removeEventListener("botpressWidgetReady", initBotpress);
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
     };
   }, []);
 
@@ -37,16 +41,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body style={{ margin: 0, padding: 0 }}>
         {children}
-
-        {/* Botpress Integration Webchat Scripts */}
-        <Script 
-          src="https://cdn.botpress.cloud/webchat/v2.2/shareable.js" 
-          strategy="lazyOnload"
-        />
-        <Script 
-          src="https://styler-e59b32.botpress.cloud/webchat/v2.2/bundle.js" 
-          strategy="lazyOnload"
-        />
       </body>
     </html>
   );
